34cdfad7ba72031b76877ce0b855d91b0506c1fd
Taras Bilohan <taras.bilohan@gmail.com>
2021-04-22 10:56:51 +0300

    Add vuetify, @vue/composition-api, axios, qs

--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
 import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import VueCompositionAPI from '@vue/composition-api'
+
+import App from '@/App.vue'
+import router from '@/router'
+import store from '@/store'
+import vuetify from '@/plugins/vuetify'
 
 Vue.config.productionTip = false
+
+Vue.use(VueCompositionAPI)
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  vuetify,
+  render: (h) => h(App)
 }).$mount('#app')
